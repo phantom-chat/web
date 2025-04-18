6153550f91aac7efb4bdef7e5da74ebffae6ddca
Lucas Kouzoukian <contato.upenha@gmail.com>
2025-04-18 14:51:58 -0300

    refactor(theme-switcher): use transition for theme change and update icon sizing

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -1,22 +1,26 @@
 "use client";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
+import { useTransition } from "react";
 import { Button } from "./ui/button";
 
 export const ThemeSwitcher = () => {
-	const { setTheme } = useTheme();
-	const toggleTheme = () => {
-		setTheme((theme) => (theme === "dark" ? "light" : "dark"));
-	};
+	const { setTheme, resolvedTheme } = useTheme();
+	const [, startTransition] = useTransition()
+
 	return (
 		<Button
 			className="fixed right-4 bottom-4"
-			onClick={toggleTheme}
+			onClick={() => {
+				startTransition(() => {
+					setTheme(resolvedTheme === "dark" ? "light" : "dark")
+				})
+			}}
 			variant="outline"
 			size="icon"
 		>
-			<Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-			<Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+			<Sun className="size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+			<Moon className="absolute size-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
 			<span className="sr-only">Toggle theme</span>
 		</Button>
 	);
